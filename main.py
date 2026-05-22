cd8d5030f0bc5e7a5c29718eb8ad18543c1aff3f
rlawodus441131-cell <rlawodus441131@gmail.com>
2026-05-22 16:20:49 +0000

    60212481-김재연-3차 과제 코드

--- main.py
+++ main.py
@@ -34,3 +34,69 @@ def show_traders():
     for i, trade in enumerate(trade_history):
         name, buy, sell, amount = trade
         print(f"{i+1:<5} {name:<12} {buy:<10.0f} {sell:<10.0f} {amount:<6.0f}")
+# 수익률 계산 후 반환
+def calc_profit_rate(buy, sell):
+    if buy == 0:
+        return 0.0  
+    profit_rate = ((sell - buy) / buy) * 100
+    return profit_rate 
+#등급 및 칭호 판정
+def get_grade(profit_rate, invest_amount):
+    if profit_rate >= 20 and invest_amount >= 100000:
+        grade = 'S'
+        title = '시장의 지배자 (고액 거물)'
+    elif profit_rate >= 20:
+        grade = 'S'
+        title = '전설의 트레이더'
+    elif profit_rate >= 10:
+        grade = 'A'
+        title = '전문 트레이더'
+    elif profit_rate >= 0:
+        grade = 'B'
+        title = '안정적 트레이더'
+    else:
+        grade = 'F'
+        title = '위험한 투자자'
+    return grade, title  #튜플로 반환
+#수익률 분석 및 리포트 출력
+def show_analysis():
+    print('\n--- 📊 수익률 분석 리포트 ---')
+    analysis_data = trade_history[:]  # 데이터 복사 (원본 보호)
+ 
+    for trade in analysis_data:
+        name, buy, sell, amount = trade
+ 
+        profit_rate = calc_profit_rate(buy, sell) 
+        invest_amount = buy * amount
+ 
+        grade, title = get_grade(profit_rate, invest_amount) 
+ 
+        print(f"종목: {name:10} | 수익률: {profit_rate:6.2f}% | 등급: [{grade}] | 칭호: {title}")
+ 
+    print(f'\n총 투자 누적액: {total_sum:,.0f}원')
+#메인 로직(whie True - 무한 루프)
+print('===================================')
+print('   ---My Quant Log : 매매 데이터 분석기---')
+print('===================================')
+ 
+while True:
+    print('\n======= 📌 메인 메뉴 =======')
+    print('1. 매매 기록 입력')
+    print('2. 전체 기록 조회')
+    print('3. 수익률 분석')
+    print('4. 종료')
+    print('============================')
+ 
+    choice = input('메뉴를 선택하세요 (1~4): ')
+ 
+    if choice == '1':
+        input_trade()       # 함수 호출
+    elif choice == '2':
+        show_trades()       # 함수 호출
+    elif choice == '3':
+        show_analysis()     # 함수 호출
+    elif choice == '4':
+        print('\n👋 프로그램을 종료합니다. 성공적인 투자 되세요!')
+        break               # 종료 메뉴 선택 시 break
+    else:
+        print('⚠️ 1~4 중에서 선택해 주세요.')
